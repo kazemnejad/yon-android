b0f23a9075eda4d8ff4f0c61a3bd2a45eee08803
AmirHosein KazemNejad <ah.kazemnejad@gmail.com>
2017-06-06 18:25:33 +0430

    Change utils directory name to util

--- app/src/main/java/io/yon/android/util/DrawerHelper.java
+++ app/src/main/java/io/yon/android/util/DrawerHelper.java
@@ -1,4 +1,4 @@
-package io.yon.android.utils;
+package io.yon.android.util;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
--- app/src/main/java/io/yon/android/util/RxBus.java
+++ app/src/main/java/io/yon/android/util/RxBus.java
@@ -1,4 +1,4 @@
-package io.yon.android.utils;
+package io.yon.android.util;
 
 import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
