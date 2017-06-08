bcfae8640da16a6d25163c53849847eee76b824d
AmirHosein KazemNejad <ah.kazemnejad@gmail.com>
2017-06-08 11:44:48 +0430

    Add better logging lib

--- app/src/main/java/io/yon/android/MainApplication.java
+++ app/src/main/java/io/yon/android/MainApplication.java
@@ -2,9 +2,21 @@ package io.yon.android;
 
 import android.app.Application;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by amirhosein on 5/27/17.
  */
 
 public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initialize();
+    }
+
+    private void initialize(){
+        Logger.addLogAdapter(new AndroidLogAdapter());
+    }
 }
