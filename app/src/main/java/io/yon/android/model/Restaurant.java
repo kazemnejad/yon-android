fd07fa8d18b6b9f1aa5ba10e779c1e25d1cb36c2
AmirHosein KazemNejad <ah.kazemnejad@gmail.com>
2017-08-01 15:15:33 +0430

    Complete Restaurant model json properties

--- app/src/main/java/io/yon/android/model/Restaurant.java
+++ app/src/main/java/io/yon/android/model/Restaurant.java
@@ -1,9 +1,13 @@
 package io.yon.android.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by amirhosein on 7/22/17.
  */
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Restaurant extends Model {
     private String name;
     private String avatarUrl;
@@ -14,6 +18,7 @@ public class Restaurant extends Model {
         return name;
     }
 
+    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
@@ -22,6 +27,7 @@ public class Restaurant extends Model {
         return avatarUrl;
     }
 
+    @JsonProperty("avatarUrl")
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
     }
@@ -30,6 +36,7 @@ public class Restaurant extends Model {
         return rate;
     }
 
+    @JsonProperty("rate")
     public void setRate(String rate) {
         this.rate = rate;
     }
@@ -38,6 +45,7 @@ public class Restaurant extends Model {
         return address;
     }
 
+    @JsonProperty("address")
     public void setAddress(String address) {
         this.address = address;
     }
