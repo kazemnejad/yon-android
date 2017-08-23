bfdb4903b65abb8d7fd48c663fe2f0caea7463e5
AmirHosein KazemNejad <ah.kazemnejad@gmail.com>
2017-08-23 10:22:39 +0430

    Fix keyboard adjust resize not working

--- app/src/main/java/io/yon/android/view/fragment/ReservationConfirmFragment.java
+++ app/src/main/java/io/yon/android/view/fragment/ReservationConfirmFragment.java
@@ -1,5 +1,13 @@
 package io.yon.android.view.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+
 import io.yon.android.R;
 
 /**
@@ -7,8 +15,29 @@ import io.yon.android.R;
  */
 
 public class ReservationConfirmFragment extends Fragment {
+
+    private ImageView userAvatar;
+
     @Override
     protected int getResourceLayoutId() {
         return R.layout.fragment_reservation_confitm;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        userAvatar.setImageDrawable(
+                TextDrawable.builder()
+                        .buildRound("ا", generator.getRandomColor())
+        );
+    }
+
+    @Override
+    protected void findViews(View v) {
+        userAvatar = (ImageView) v.findViewById(R.id.user_avatar);
+    }
+
+
 }
